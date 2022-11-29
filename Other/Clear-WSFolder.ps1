2193ed054cce5bbab0f9025a45d920cabaa8d62c
Zakhar Kvasha <kvasha97@gmail.com>
2022-11-29 18:42:44 +0400

    New script for delteing old files from folder

---
+++ Other/Clear-WSFolder.ps1
@@ -0,0 +1,34 @@
+﻿[cmdletbinding(SupportsShouldProcess)]
+param(
+    [Parameter(Mandatory=$true)]
+    [ValidateScript ({Test-Path $_ -PathType Container})] $Folder,
+    
+    [ValidateSet("CreationTime","AccessTime")]
+    [Parameter(Mandatory=$true)] [string]$Compare,
+
+    [Parameter(Mandatory=$true)] [int] $Days
+
+)
+
+$Folder = Get-Item $Folder
+$Files = @(Get-Item "$($Folder.FullName)\*")
+
+foreach ($file in $Files) {
+    
+        switch ($Compare){
+            CreationTime {
+                if (((Get-Date) - $file.CreationTime).Days -gt $Days) {
+                    Remove-Item $file -Recurse
+                }
+            }
+
+            AccessTime {
+                if (((Get-Date) - $file.LastAccessTime).Days -gt $Days) {
+                    Remove-Item $file -Recurse
+                }
+            }
+
+            Default {Write-Error "Wrong compare value"}
+        }
+}
+return
